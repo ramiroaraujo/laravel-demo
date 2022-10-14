364d493438033d35a2a1c15bd9e338384f3a707d
Ramiro Araujo <rama.araujo@gmail.com>
2022-10-14 02:50:56 -0300

    factories and seeding

---
+++ database/factories/ThreadFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Thread>
+ */
+class ThreadFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title' => fake()->realTextBetween(20, 50),
+            'user_id' => User::factory(),
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Message;
+use App\Models\Thread;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +17,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $users = User::factory(fake()->numberBetween(3, 5))->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $threads = $users->map(fn($user) => Thread::factory(fake()->numberBetween(2, 6))->create(['user_id' => $user->id]))->flatten();
+
+        $threads->each(fn($thread) => Message::factory(fake()->numberBetween(10, 20))->create(['thread_id' => $thread->id, 'user_id' => $users->random()->id]));
     }
 }
